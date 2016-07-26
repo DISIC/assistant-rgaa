bf2a1a6144a59fe2cddb7903f7e7413744940b87
Emmanuel Pelletier <e.pelletier@vtech.fr>
2016-07-26 17:42:35 +0200

    styling Test component
    They look like a table, they are a table, but we can't simply render
    them with react so let's stay like that for now.

--- src/panel/js/components/Test.js
+++ src/panel/js/components/Test.js
@@ -1,53 +1,78 @@
 import React, {PropTypes} from 'react';
 import {FormattedMessage, injectIntl, intlShape} from 'react-intl';
 import renderIf from 'render-if';
-import {noop} from 'lodash';
+import {noop, random} from 'lodash';
 
 
 
 /**
  *
  */
-function Test({title, instructions, helpers, applied, done, onApply, onDone, intl}) {
+function Test({id, title, instructions, helpers, applied, done, onApply, onDone, intl}) {
+
 	const handleApplyChange = (event) =>
 		onApply(event.target.checked, helpers);
 
 	const handleDoneChange = (event) =>
 		onDone(event.target.checked);
 
+	const randomApplied = random(0, 1, false); // for testing while applied is not implemented
+
+	const applyImage = randomApplied ? 'prohibition.png' : 'magnifier-zoom.png';
+	const applyTranslateKey = randomApplied ? 'uncheck' : 'check';
+
 	return (
 		<article className="Test">
 			<header className="Test-header">
 				<h2 className="Test-title">{title}</h2>
 
-				<input
-					className="Test-apply"
-					type="checkbox"
-					title={intl.formatMessage({
-						id: 'Test.apply'
-					})}
-					checked={applied}
-					onChange={handleApplyChange}
-				/>
+				<div className="Test-actions">
+					<div className="Test-action Test-action---apply">
+						<label
+							htmlFor={`test-${id}-apply-input`}
+							className="Test-actionLabel"
+							title={intl.formatMessage({
+								id: `Test.apply.${applyTranslateKey}.title`
+							})}
+						>
+							<img
+								src={`/img/${applyImage}`}
+								alt={intl.formatMessage({
+									id: `Test.apply.${applyTranslateKey}.alt`
+								})}
+							/>
+						</label>
+						<input
+							className="Test-actionInput u-hidden"
+							type="checkbox"
+							id={`test-${id}-apply-input`}
+							checked={randomApplied}
+							onChange={handleApplyChange}
+						/>
+					</div>
 
-				<input
-					className="Test-done"
-					type="checkbox"
-					title={intl.formatMessage({
-						id: 'Test.done'
-					})}
-					checked={done}
-					onChange={handleDoneChange}
-				/>
+					<div className="Test-action Test-action---done">
+						<input
+							className="Test-actionInput"
+							type="checkbox"
+							title={intl.formatMessage({
+								id: 'Test.done'
+							})}
+							checked={done}
+							onChange={handleDoneChange}
+						/>
+					</div>
+				</div>
 			</header>
 
 			{renderIf(instructions)(() => (
-				<details>
-					<summary>
+				<details className="Test-instructions">
+					<summary className="Test-instructionsTitle">
 						<FormattedMessage id="Test.instructions" />
 					</summary>
 
 					<div
+						className="Test-instructionsText"
 						dangerouslySetInnerHTML={{
 							__html: instructions
 						}}
@@ -59,6 +84,7 @@ function Test({title, instructions, helpers, applied, done, onApply, onDone, int
 }
 
 Test.propTypes = {
+	id: PropTypes.string.isRequired,
 	title: PropTypes.string.isRequired,
 	instructions: PropTypes.string,
 	helpers: PropTypes.array,
--- src/panel/js/messages/fr.js
+++ src/panel/js/messages/fr.js
@@ -5,7 +5,10 @@ const messages = {
 	'Header.title': 'Panel d\'outils RGAA',
 	'Theme.criterion.select.label': 'Aller au critère : ',
 	'Theme.criterion.select.emptyOption': 'Choisissez un critère',
-	'Test.apply': 'Appliquer / enlever les CSS et JS liés au test',
+	'Test.apply.check.title': 'Appliquer les CSS et JS liés au test',
+	'Test.apply.uncheck.title': 'Enlever les CSS et JS liés au test',
+	'Test.apply.check.alt': 'Loupe',
+	'Test.apply.uncheck.alt': 'Interdit',
 	'Test.done': 'Test effectué',
 	'Test.instructions': 'Instructions'
 };
