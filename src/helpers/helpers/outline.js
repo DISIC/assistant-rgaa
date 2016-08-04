e996a719f8491700b3903da69aba2823f38e70a3
Félix Girault <felix.girault@gmail.com>
2016-08-04 16:14:34 +0200

    Using outlines instead of borders

--- src/helpers/helpers/outline.js
+++ src/helpers/helpers/outline.js
@@ -3,19 +3,19 @@ import {apply as addClassName, revert as removeClassName} from './addClassName';
 
 
 /**
- *	Adds a border to each element matched by the given selector.
+ *	Adds an outline to each element matched by the given selector.
  *
  *	@param {string} id - UUID.
  *	@param {string} selector - Selector.
  */
 export const apply = (id, selector) =>
-	addClassName(id, selector, 'rgaaExt-BorderHelper');
+	addClassName(id, selector, 'rgaaExt-OutlineHelper');
 
 /**
- *	Removes borders that were previously disabled using apply().
+ *	Removes outlines that were previously disabled using apply().
  *
  *	@param {string} id - UUID.
  *	@param {string} selector - Selector.
  */
 export const revert = (id, selector) =>
-	removeClassName(id, selector, 'rgaaExt-BorderHelper');
+	removeClassName(id, selector, 'rgaaExt-OutlineHelper');
