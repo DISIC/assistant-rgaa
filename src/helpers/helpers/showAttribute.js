e464cbff1e2e341449d308f3ea21ae3bcc585645
Félix Girault <felix.girault@gmail.com>
2016-08-04 14:09:56 +0200

    Added links to id references in attributes
    So that one can click on ids to jump on the referenced node

--- src/helpers/helpers/showAttribute.js
+++ src/helpers/helpers/showAttribute.js
@@ -11,7 +11,60 @@ import $ from 'jquery';
 
 
 /**
+ *	Attributes that contain a list of ids.
+ */
+const LINK_ATTRIBUTES = [
+	'for',
+	'aria-labelledby'
+];
+
+
+
+/**
+ *	If an attribute with the given name contains a list of id,
+ *	makes a anchor to each of the linked ids.
  *
+ *	@param {string} name - Attribute name.
+ *	@param {string} value - Attribute value.
+ *	@return {string} - Attribute value containing anchors if needed.
+ */
+const linkIds = (name, value) => {
+	if (LINK_ATTRIBUTES.includes(name)) {
+		return chain(value)
+			.split(/\s+/)
+			.map((id) => `<a href="#${id}">#${id}</a>`)
+			.join(' ')
+			.value();
+	}
+
+	return value;
+};
+
+/**
+ *	Builds HTML code to render an attribute's name and value.
+ *
+ *	@param {string} name - Attribute name.
+ *	@param {string} value - Attribute value.
+ *	@return {string} - HTML.
+ */
+const attributeBox = (name, value) => {
+	if (value === undefined) {
+		return `<span class="rgaaExt-ShowAttributeHelper-missing">${name}</span>`;
+	}
+
+	return `<span class="rgaaExt-ShowAttributeHelper-name">${name}</span>`
+		+ `="<span class="rgaaExt-ShowAttributeHelper-value">${linkIds(name, value)}</span>"`;
+};
+
+
+
+/**
+ *	Shows a box containing an attribute's name and value on
+ *	each element matched by the given selector.
+ *
+ *	@param {string} id - UUID.
+ *	@param {string} selector - Selector.
+ *	@param {string} attribute - Attribute name.
  */
 export const apply = (id, selector, attribute) =>
 	$(selector).each((i, el) => {
@@ -21,9 +74,7 @@ export const apply = (id, selector, attribute) =>
 		$(element).after(
 			$('<p />', {
 				class: `${id} rgaaExt-Helper rgaaExt-ShowAttributeHelper`,
-				text: value
-					? `${attribute}="${value}"`
-					: `Pas d'attribut ${attribute}`
+				html: attributeBox(attribute, value)
 			})
 		);
 	});
