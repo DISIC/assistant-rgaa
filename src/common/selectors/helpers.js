a6df80eee7036061a290605f9b02db12b71a6c1c
Félix Girault <felix.girault@gmail.com>
2016-08-11 18:20:17 +0200

    Hiding the apply button when a test has no helpers

--- src/common/selectors/helpers.js
+++ src/common/selectors/helpers.js
@@ -7,3 +7,9 @@ import {get} from 'lodash';
  */
 export const getHelpersByTest = (state, id) =>
 	get(state, ['helpers', id], []);
+
+/**
+ *
+ */
+export const testHasHelpers = (state, id) =>
+	(getHelpersByTest(state, id).length > 0);
