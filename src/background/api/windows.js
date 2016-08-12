2b83ecd566b83f88d205ffcaf1124d15f1b11288
Emmanuel Pelletier <e.pelletier@vtech.fr>
2016-08-12 11:34:37 +0200

    windows api: catch chrome runtime errors + add a get function

--- src/background/api/windows.js
+++ src/background/api/windows.js
@@ -2,14 +2,40 @@
  *
  */
 export const create = (options) =>
-	new Promise(resolve => // eslint-disable-line no-new
-		chrome.windows.create(options, resolve)
+	new Promise((resolve, reject) => // eslint-disable-line no-new
+		chrome.windows.create(options, (newWindow) => {
+			if (chrome.runtime.lastError) {
+				reject(chrome.runtime.lastError);
+			} else {
+				resolve(newWindow);
+			}
+		})
 	);
 
 /**
  *
  */
 export const remove = (id) =>
-	new Promise((resolve) => // eslint-disable-line no-new
-		chrome.windows.remove(id, resolve)
+	new Promise((resolve, reject) => // eslint-disable-line no-new
+		chrome.windows.remove(id, () => {
+			if (chrome.runtime.lastError) {
+				reject(chrome.runtime.lastError);
+			} else {
+				resolve();
+			}
+		})
+	);
+
+/**
+ *
+ */
+export const get = (id, options = {populate: true}) =>
+	new Promise((resolve, reject) => // eslint-disable-line no-new
+		chrome.windows.get(id, options, (windowInfo) => {
+			if (chrome.runtime.lastError) {
+				reject(chrome.runtime.lastError);
+			} else {
+				resolve(windowInfo);
+			}
+		})
 	);
