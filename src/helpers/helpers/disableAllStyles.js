06a750b236c928365a2476f792732e323297a920
Félix Girault <felix.girault@gmail.com>
2017-05-03 11:43:42 +0200

    Capping images' size when disabling styles

--- src/helpers/helpers/disableAllStyles.js
+++ src/helpers/helpers/disableAllStyles.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import toggleStyleSheets from '../api/toggleStyleSheets';
 import {mutedAttributeSelector, muteAttribute, restoreAttribute} from '../api/muteAttributes';
+import {apply as applyStyle, revert as revertStyle} from './style';
 
 
 
@@ -17,18 +18,29 @@ export const describe = (intl) =>
 /**
  *	Disable all style sheets in the page.
  */
-export const apply = () => {
+export const apply = (id) => {
 	toggleStyleSheets(false);
 	muteAttribute($('[style]'), 'style');
+	applyStyle(id, {
+		style: `
+			img, svg, embed[type="image"], object[type="image"] {
+				width: auto !important;
+				height: auto !important;
+				max-width: 400px !important;
+				max-height: 400px !important;
+			}
+		`
+	});
 };
 
 /**
  *	Enable all style sheets that were previously disabled using
  *	apply().
  */
-export const revert = () => {
+export const revert = (id) => {
 	const selector = mutedAttributeSelector('style');
 
-	toggleStyleSheets(true);
+	revertStyle(id);
 	restoreAttribute($(selector), 'style');
+	toggleStyleSheets(true);
 };
